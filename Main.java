fb32722bd5d964dcb9fc86bbe9f000bbfcb21fa7
torbuntu <sorensentor@tuta.io>
2019-09-15 16:29:53 -0500

    Initial project work. Adding Tor asset with dashing animation. First GIF

--- Main.java
+++ Main.java
@@ -2,14 +2,19 @@ import femto.mode.HiRes16Color;
 import femto.Game;
 import femto.State;
 import femto.input.Button;
-import femto.palette.Psygnosia;
+import femto.palette.GrungeShift;
 import femto.font.TIC80;
+
+
+import Tor;
 
 class Main extends State {
 
     HiRes16Color screen; // the screenmode we want to draw with
 
-    Dog dog; // an animated sprite imported from Aseprite
+
+    Tor tor;
+
     Pattern background; // static image
     float angle; // floats are actually FixedPoint (23.8)
     int dxs, dys;
@@ -24,11 +29,13 @@ class Main extends State {
     // Avoid allocation in a State's constructor.
     // Allocate on init instead.
     void init(){
-        screen = new HiRes16Color(Psygnosia.palette(), TIC80.font());
+        screen = new HiRes16Color(GrungeShift.palette(), TIC80.font());
         background = new Pattern();
-        dog = new Dog();
-        dog.run(); // "run" is one of the animations in the spritesheet
         speed = 0;
+        
+        tor = new Tor();
+        tor.y = 70;
+        tor.dash();
     }
     
     // Might help in certain situations
@@ -38,6 +45,7 @@ class Main extends State {
     
     // update is called by femto.Game every frame
     void update(){
+        screen.clear(0);
         dys = 0;
         dxs = 0;
         if(Button.Up.isPressed()) dys = -1;
@@ -50,18 +58,16 @@ class Main extends State {
             Game.changeState( new Main() );
 
         // Fill the screen using Pattern.png
-        for( int y=0; y<176; y += background.height() ){
             for( int x=0; x<440; x += background.width() ){
                 if(x+speed < -background.width()*2) speed = 0;
-                background.draw(screen, x+speed, y);
+                background.draw(screen, x+speed, 100);
             }
-        }
         speed--;
         counter++;
         
-        dog.x += dxs;
-        dog.y += dys;
-        dog.draw(screen); // Animation is updated automatically
+        tor.x += dxs;
+        tor.y += dys;
+        tor.draw(screen); // Animation is updated automatically
         
         // Update the screen with everything that was drawn
         screen.flush();
